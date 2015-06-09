fea4c76a4eef015b9bb566aeca8febef90f6cae8
Julien Christin <jul.christin@gmail.com>
2015-06-09 17:10:32 -0400

    Implement a basic API (build, save, load)

---
+++ bin/graph.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var path = require("path"),
+	osmToGraph = require("../lib/index.js"),
+	argv = process.argv.slice(2);
+
+if (argv.length !== 1) {
+	throw new Error("Invalid parameters.");
+}
+
+var extname = path.extname(argv[0]);
+if (extname !== ".osm") {
+	throw new Error("Input file must be an OSM file.");
+}
+
+var basename = path.basename(argv[0], extname);
+var graph = osmToGraph.buildGraph(argv[0]);
+osmToGraph.displayGraphInfo(graph);
+osmToGraph.saveGraph(graph, basename + ".json");
